55f0dec25f8dfc068c435bc1dbce60f3e1bea26a
Joshua Macklin <69019114+JoshuaMacklin@users.noreply.github.com>
2020-09-21 01:20:00 -0700

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : `mongodb+srv://test:test@cluster0.scn8b.mongodb.net/Expenses?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'Expenses'
+};
